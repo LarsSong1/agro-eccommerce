d7eac5693c6f6bd8a7ba11c4417eac2dfc222513
LarsSong1 <damianalejandrofirst@gmail.com>
2024-06-18 04:16:45 -0500

    yetdesign11

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const Layout = () => {
             <Drawer />
             <Navbar />
             <Outlet />
-            {/* <ScrollRestoration /> */}
+            <ScrollRestoration />
             <Footer />
 
           </section>
